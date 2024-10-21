712224d56049d7448fa22b2f7fee16197aec4c10
zyh <zhang.yuh@outlook.com>
2024-10-21 18:13:27 +0800

    install dep

---
+++ install.sh
@@ -0,0 +1,22 @@
+# conda create -n tpv python=3.8
+
+conda install pytorch==1.10.0 torchvision==0.11.0 torchaudio==0.10.0 -c pytorch -y
+
+pip install openmim 
+
+pip install mmcv-full==1.4.0 
+
+pip install mmdet==2.14.0 
+
+pip install mmsegmentation==0.14.1 
+
+
+pip install numba==0.48.0 
+
+pip install numpy==1.23.1 
+
+pip install Pillow==9.3.0 
+
+pip install PyYAML==6.0 
+
+pip install timm==0.4.12 
